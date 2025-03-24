81ffb6f5cc406c304efb169178683ae4af9157e3
EnzoEduardo-svg <enzoe3131e@gmail.com>
2025-03-24 15:26:52 +0000

    q7-8

--- ola.py
+++ ola.py
@@ -1,3 +1,13 @@
 print('olá mundo!!!')
-git add ola.py
 
+
+salvando arquivos
+git add *.py
+git commit -m "mensagem do que foi alterado"
+git push
+repositório do Professor
+https://github.com/ifmt-cba/lp20251
+#salvando arquivos do projeto
+
+git commit -m "mensagem do que foi alterado"
+git push
